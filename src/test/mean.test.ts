875e4642ba3a26ec8620a0a062f21773b6685019
Bryan Bell <bryanbellabc@gmail.com>
2023-08-12 12:48:07 -0600

    feat: :sparkles: Calculate Mean returns the right value.

--- src/test/mean.test.ts
+++ src/test/mean.test.ts
@@ -5,6 +5,11 @@
 
     // Triple AAA
     // Arrange, Act, Assert
+
+    // Happy Path
+    // Edge Cases
+
+    // "Change is the enemy of good software design"
 */
 
 import { it, expect } from 'vitest';
@@ -16,7 +21,32 @@ it('should return a number', () => {
     const expectedType = 'number';
 
     // Act
-    const result = calculateMean([1, 2, 4]); 
-        
+    const result = calculateMean([1, 2, 4]);
+
+    // Assert    
     expect(result).toBeTypeOf(expectedType);
+})
+
+it('should return a number', () => {
+    // Arrange
+    const expected = 3;
+    const nums = [1, 2, 3, 4, 5];
+
+    // Act
+    const result = calculateMean(nums);
+
+    // Assert
+    expect(result).toBe(expected);
+})
+
+it('should return a number', () => {
+    // Arrange
+    const expected = 6;
+    const nums = [2, 4, 6, 8, 10];
+
+    // Act
+    const result = calculateMean(nums);
+
+    // Assert
+    expect(result).toBe(expected);
 })
--- src/test/mean.ts
+++ src/test/mean.ts
@@ -1,3 +1,10 @@
-export function calculateMean() {
-    return 2;
+export function calculateMean(nums: number[]): number {
+    let sum = 0;
+    
+    for(let num of nums) {
+        sum += num;
+    }
+
+    const result = sum / nums.length
+    return result;
 }
