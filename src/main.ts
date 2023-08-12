5bd4a7530f4ca58c7a6cfddb2133135b90bd955b
Bryan Bell <bryanbellabc@gmail.com>
2023-08-12 15:17:05 -0600

    docs: :memo: Add some examples about interfaces.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,79 @@
-console.log("Hi");
+// Tipado de Primitivos
+// string, number, boolean
+
+// let age: number = 22;
+// const cousinName = "Marlon";
+// const isMarried = false;
+
+// interface User {
+//     name: string;
+//     age: number;
+// }
+
+// const user: User = {
+//     age: 12,
+//     name: 'err'
+// }
+
+// const users: User[] = [
+//     {
+//         name: 'Bryan',
+//         age: 22,
+//     },
+//     {
+//         name: 'Marlon',
+//         age: 39
+//     },
+//     {
+//         name: 'erick',
+//         age: 24,
+//     }
+// ];
+
+interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+}
+
+async function getUsers() {
+    const URL = "https://jsonplaceholder.typicode.com/users";
+    const resp = await fetch(URL);
+    const usersData = await resp.json() as User[];
+
+    return usersData;
+}
+
+const users = await getUsers();
+
+users.map( user => {
+    user.email
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {};
